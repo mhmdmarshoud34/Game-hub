06780394dca8483db252700325ce90219f7d1a5f
mhmdmarshoud34 <mhmdmarshoud34@gmail.com>
2024-09-06 18:01:57 +0300

    Refactor remove duplicated style

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow={"hidden"} width="300px">
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="1.75xl">{game.name}</Heading>
