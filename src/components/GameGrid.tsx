620a966553a71c0fd258469f000dbed6e8aaa354
mhmdmarshoud34 <mhmdmarshoud34@gmail.com>
2024-09-07 19:14:18 +0300

    display genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,6 +15,7 @@ export const GameGrid = () => {
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={10}
         padding={10}
+        spacingX={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
